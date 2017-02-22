a6bf8d05e61810cc1b33a4b7e3b80717776c2406
aviv0404 <aviv040400@gmail.com>
2017-02-22 20:11:19 +0200

    Add files via upload

---
+++ current_code/MyBot.java
@@ -0,0 +1,60 @@
+package bots;
+import pirates.*;
+
+/**
+ * This is an example for a bot.
+ */
+public class MyBot implements PirateBot {
+
+    /**
+     * Makes the bot run a single turn
+     *
+     * @param game - the current game state.
+     * 
+     */
+     
+    @Override
+    public void doTurn(PirateGame game) {
+        if(firstTurn()) {
+            initFirstTurn();
+        }
+        
+        saveHistory();
+        Strategy strategy = decideStrategy();
+        strategy.doTurn(game);
+    }
+
+    /**
+     * Gives orders to my pirates
+     *
+     * @param game - the current game state
+     */
+    private void handlePirates(PirateGame game) {
+        // Go over all of my pirates
+        for (Pirate pirate : game.getMyLivingPirates()) {
+            // Try to attack, if you didn't - move to an island
+            if (!Attacker.tryAttack(pirate, game)) {
+                // Choose destination
+                Island destination = game.getAllIslands().get(0);
+                // Send to destination
+				Mover.moveAircraft(pirate, destination, game);
+            }
+        }
+    }
+
+    /**
+     * Gives orders to my drones
+     *
+     * @param game - the current game state
+     */
+    private void handleDrones(PirateGame game) {
+        // Go over all of my drones
+        for (Drone drone : game.getMyLivingDrones()) {
+            // Choose a destination
+            City destination = game.getMyCities().get(0);
+            // Send to destination game)
+			Mover.moveAircraft(drone, destination, game);
+        }
+    }
+}
+
