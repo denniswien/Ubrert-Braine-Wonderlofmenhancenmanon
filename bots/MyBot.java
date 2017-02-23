59aec2c341e99a3153892aaaf1f6839c9eaa9116
aviv0404 <aviv040400@gmail.com>
2017-02-23 20:39:49 +0200

    Update MyBot.java (#9)

--- bots/MyBot.java
+++ bots/MyBot.java
@@ -5,7 +5,6 @@ import pirates.*;
  * This is an example for a bot.
  */
 public class MyBot implements PirateBot {
-    private History history;
 
     /**
      * Makes the bot run a single turn
@@ -13,25 +12,72 @@ public class MyBot implements PirateBot {
      * @param game - the current game state.
      * 
      */
+     
     @Override
     public void doTurn(PirateGame game) {
-        if(firstTurn()) {
+        /*if(firstTurn()) {
             initFirstTurn();
         }
-
-        history.update(game);
-        Strategy strategy = decideStrategy().doTurn(history);
+        
+        saveHistory();
+        Strategy strategy = decideStrategy();
+        strategy.doTurn(game);
+        */
+        if (game.getEnemyCities().size() == 0){
+            attack();
+        }
+        if (game.getMyCities().size() == 0){
+            defense();
+        }
+        if (game.getMyCities().size() > && game.getEnemyCities().size() > 0){
+            specialStrategy();
+        }
     }
 
-    private boolean firstTurn() {
-        return game.getTurnNumber() == 0;
+    
+    private void defense(){
+        
+    }
+    
+    //1 pirate that sends drones and the other attack
+    private void attack(){
+        
+    }
+    
+    private void specialStrategy(){
+        
+    }
+    /**
+     * Gives orders to my pirates
+     *
+     * @param game - the current game state
+     */
+    private void handlePirates(PirateGame game) {
+        // Go over all of my pirates
+        for (Pirate pirate : game.getMyLivingPirates()) {
+            // Try to attack, if you didn't - move to an island
+            if (!Attacker.tryAttack(pirate, game)) {
+                // Choose destination
+                Island destination = game.getAllIslands().get(0);
+                // Send to destination
+				Mover.moveAircraft(pirate, destination, game);
+            }
+        }
     }
 
-    private void initFirstTurn() {
-        history = new History();
-        //TODO(implement): (for heuristic strategy) add measurements 2017-02-22 20:07:12 (amit)
+    /**
+     * Gives orders to my drones
+     *
+     * @param game - the current game state
+     */
+    private void handleDrones(PirateGame game) {
+        // Go over all of my drones
+        for (Drone drone : game.getMyLivingDrones()) {
+            // Choose a destination
+            City destination = game.getMyCities().get(0);
+            // Send to destination game)
+			Mover.moveAircraft(drone, destination, game);
+        }
     }
-
-    private Strategy decideStrategy(History history) {}
 }
 
