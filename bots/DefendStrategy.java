d8c6168746358a9939619969435de13490a46c0f
hodvaknin <hodvak@walla.com>
2017-02-24 13:31:52 +0200

    Update DefendStrategy.java

--- bots/DefendStrategy.java
+++ bots/DefendStrategy.java
@@ -1,12 +1,12 @@
 package bots;
 
-import java.util.*
-import pirates.game.*
-import java.lang.Math
+import java.util.*;
+import pirates.game.*;
+/*import java.lang.Math
 import java.lang.reflect
 import java.lang.class
 import java.math
-
+*/
 
 class DefendStrategy implements Strategy {
 
@@ -21,13 +21,41 @@ class DefendStrategy implements Strategy {
         int numMyLivingPirates = myLivingPirates.size();
         game.debug(numMyLivingPirates);
         for (int i = 0; i < numMyLivingPirates; i++) {
-            if (!Attacker.tryAttack(myLivingPirates.get(i), game)) {
-                Mover.moveAircraftToClosestToAnotherMapObject(
+            if (Attacker.tryAttack(myLivingPirates.get(i), game)) 
+                myLivingPirates.remove(i);
+        }        /*Mover.moveAircraftToClosestToAnotherMapObject(
                                 myLivingPirates.get(i),
                                 enemyLivingDrones,
                                 enemyCities.get(0),
-                                game);
-            }
+                                game);*/
+        numMyLivingPirates = myLivingPirates.size();
+        for(int i = 0; i < numMyLivingPirates; i++)
+        {
+                Drone closest = (Drone)getColosest(enemyCities.get(0), enemyLivingDrones);
+                enemyLivingDrones.remove(closest);
+                if(closest == null)
+                {
+                    //for(Pirate pirate:myLivingPirates)
+                    //    Mover.moveAircraft(pirate,enemyCities.get(0),game);
+                    return;
+                }
+                Pirate pirate = (Pirate)getColosest(closest, myLivingPirates);
+                myLivingPirates.remove(pirate);  
+                Mover.moveAircraft(pirate,closest,game);
         }
+        
+    }
+    public <T extends MapObject> MapObject getColosest(MapObject mapObject,List<T> list)
+    {
+        if(mapObject == null)//no target
+            return null;
+        
+        MapObject closest = null;
+        for(T t:list)
+        {
+            if(closest == null || closest.distance(mapObject) > t.distance(mapObject))
+                closest = t;
+        }
+        return closest;
     }
 }
