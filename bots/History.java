50e1998ca7c371365f3ab94a50e6916301a63725
hodvaknin <hodvak@walla.com>
2017-02-25 22:06:08 +0200

    Update History.java

--- bots/History.java
+++ bots/History.java
@@ -1,12 +1,34 @@
 package bots;
-import java.util.*
-import pirates.game.*
-import java.lang.Math
-import java.lang.reflect
-import java.lang.class
-import java.math
-
+import java.util.ArrayList;
+import java.util.List;
+import pirates.*;
 
 class History {
-    void update(PirateGame game) {}
+    private List<List<Location>> enemysPiratesLocation;
+    private List<List<Integer>> enemysPiratesHealth;
+    public History(PirateGame game)
+    {
+        enemysPiratesLocation = new ArrayList<>();
+        enemysPiratesHealth = new ArrayList<>();
+    }
+    void update(PirateGame game) 
+    {
+        enemysPiratesLocation.add(new ArrayList<>());
+        enemysPiratesHealth.add(new ArrayList<>());
+        List<Location> thisTurnEnemyPiratesLocations = enemysPiratesLocation.get(enemysPiratesLocation.size()-1);
+        List<Integer> thisTurnEnemyPiratesHealth = enemysPiratesHealth.get(enemysPiratesLocation.size()-1);
+        for (Pirate pirate : game.getEnemyLivingPirates()) {
+            thisTurnEnemyPiratesLocations.add(pirate.location);
+            thisTurnEnemyPiratesHealth.add(pirate.currentHealth);
+            
+        }
+    }
+    public List<Location> getEnemyPiratesLocatinByTurn(int turn)
+    {
+        return enemysPiratesLocation.get(turn);
+    }
+    public List<Integer> getEnemyPiratesHealthByTurn(int turn)
+    {
+        return enemysPiratesHealth.get(turn);
+    }
 }
