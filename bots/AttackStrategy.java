e6667cb3dd21d00cc63722f7c0847b1b54ea2d09
aviv0404 <aviv040400@gmail.com>
2017-02-27 19:21:08 +0200

    Hhhh (#18)
    * Create DetectDecoy.java
    
    * Update History.java
    
    * Update History.java
    
    * Update History.java
    
    * Update AttackStrategy.java

--- bots/AttackStrategy.java
+++ bots/AttackStrategy.java
@@ -1,14 +1,144 @@
 package bots;
-import java.util.*
-import pirates.game.*
-import java.lang.Math
-import java.lang.reflect
-import java.lang.class
-import java.math
 
-class AttackStrategy implements Strategy {
-    public void doTurn(PirateGame game,History history) {
+import pirates.*;
+import java.util.*;
+
+class AttackStrategy implements Strategy
+{
+
+    private boolean isCentered;
+    private boolean[] isThere;
+    private Player p;
+
+    public AttackStrategy(){}
+    
+    public AttackStrategy(boolean[] isThere)
+    {
+        this.isThere = isThere;
+    }
+
+    public void doTurn(PirateGame game, History history)
+    {
+        game.debug(game.getTurn());
+        //isThere = new boolean[game.getMaxDronesCount()];
+        /*
+        if(game.getTurn() == 1){
+            game.debug("constructor");
+            isCentered = false;
+            isThere = new boolean[game.getMaxDronesCount()];
+            for(int i = 0; i < game.getMaxDronesCount(); i++)
+                {
+                    isThere[i] = false;
+                    game.debug(isThere);
+                }
+        }*/
+        p = game.getMyself();
         game.debug("Activated: AttackStrategy");
+        Location center = new Location(game.getRowCount() / 2, game.getColCount() / 2);
+        List<Pirate> myLivingPirates = game.getMyLivingPirates();
+        List<Drone> myLivingDrones = game.getMyLivingDrones();
         
+        game.debug(isCentered);
+        isCentered = areAllTrue(isThere);
+        
+        if(!game.getNeutralIslands().isEmpty())
+        {
+
+            game.debug("1");
+            for(int i = 0; i < myLivingPirates.size(); i++)
+            {
+                Pirate pirate = myLivingPirates.get(i);
+                Mover.moveAircraft(pirate, game.getNeutralIslands().get(0), game);
+            }
+        }
+        else if(game.getTurn() < 250)
+        {
+            game.debug("2");
+            ///
+            for(int i = 0; i < myLivingPirates.size(); i++)
+            {
+                Pirate pirate = myLivingPirates.get(i);
+                Mover.moveAircraft(pirate, center, game);
+
+            }
+            game.debug(myLivingDrones.size());
+            for(int i = 0; i < myLivingDrones.size(); i++)
+            {
+                
+                Drone drone = myLivingDrones.get(i);
+                Mover.moveAircraft(drone, center, game);
+                if(drone.getLocation().compareTo(center) == 0)
+                {
+                    isThere[i] = true;
+                    //game.debug(isThere[i]);
+                }
+            }
+            isCentered = areAllTrue(isThere);
+            
+        //game.debug("is1");
+        //game.debug(isCentered);
+        
+        //game.debug(isCentered);
+        //game.debug("is2");    
+            ////
+        }else{
+            game.debug("3");
+            for(int i = 0; i < myLivingDrones.size(); i++)
+            {
+                Drone drone = myLivingDrones.get(i);
+                Mover.moveAircraft(drone, game.getMyCities().get(0).getLocation(), game);
+            }
+            
+            for(int i = 0; i < myLivingPirates.size(); i++)
+            {
+                Pirate pirate = myLivingPirates.get(i);
+                /*if(p.turnsToDecoyReload == 0 && i == 0)
+                {
+                   
+
+                else */
+                if(!Attacker.tryAttack(pirate, game))
+                {
+                    Drone closest = (Drone)Mover.getClosest(pirate.location,game.getMyLivingDrones());
+                    Mover.moveAircraft(pirate, new Location(closest.getLocation().row - 1,closest.getLocation().col - 3), game);
+                }
+
+            }
+            
+        
+        game.debug("isCentered");
+        game.debug(isCentered);
+        //end of the turn
+        /*for(int i = 0; i < game.getMaxDronesCount(); i++)
+            {
+                  
+            }*/
+            
+        }
+    }
+
+    private void initBoolArray(int size)
+    {
+        isThere = new boolean[size];
+        if(!isCentered)
+        {
+            for(int i = 0; i < size; i++)
+            {
+                isThere[i] = false;
+            }
+        }
+    }
+
+    private boolean areAllTrue(boolean[] array)
+    {
+        for(boolean b : array)
+        {
+        
+            if(!b)
+            {
+                return false;
+            }
+        }
+        return true;
     }
 }
--- bots/History.java
+++ bots/History.java
@@ -1,12 +1,42 @@
 package bots;
-import java.util.*
-import pirates.game.*
-import java.lang.Math
-import java.lang.reflect
-import java.lang.class
-import java.math
-
+import java.util.ArrayList;
+import java.util.List;
+import pirates.*;
 
 class History {
-    void update(PirateGame game) {}
+    private List<List<Location>> enemysPiratesLocation;
+    private List<List<Integer>> enemysPiratesHealth;
+    public History(PirateGame game)
+    {
+        enemysPiratesLocation = new ArrayList<>();
+        enemysPiratesHealth = new ArrayList<>();
+    }
+    void update(PirateGame game) 
+    {
+        enemysPiratesLocation.add(new ArrayList<>());
+        enemysPiratesHealth.add(new ArrayList<>());
+        List<Location> thisTurnEnemyPiratesLocations = enemysPiratesLocation.get(enemysPiratesLocation.size()-1);
+        List<Integer> thisTurnEnemyPiratesHealth = enemysPiratesHealth.get(enemysPiratesLocation.size()-1);
+        for (Pirate pirate : game.getEnemyLivingPirates()) {
+            thisTurnEnemyPiratesLocations.add(new Location(pirate.location.row,pirate.location.col));//create copy
+            thisTurnEnemyPiratesHealth.add(pirate.currentHealth);
+            
+        }
+    }
+    public List<Location> getEnemyPiratesLocatin(int turn)//return copy
+    {
+        List<Location> copy = new ArrayList<>();
+        for (Location l : enemysPiratesLocation.get(turn)) {
+            copy.add(l);
+        }
+        return copy;
+    }
+    public List<Integer> getEnemyPiratesHealth(int turn)//return copy
+    {
+        List<Integer> copy = new ArrayList<>();
+        for (Integer i : enemysPiratesHealth.get(turn)) {
+            copy.add(i);
+        }
+        return copy;
+    }
 }
