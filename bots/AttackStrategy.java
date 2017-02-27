df5327a644ed5eb5c787dc5e3fc78de1ea3944c6
hodvaknin <hodvak@walla.com>
2017-02-27 21:41:20 +0200

    Update AttackStrategy.java

--- bots/AttackStrategy.java
+++ bots/AttackStrategy.java
@@ -5,140 +5,141 @@ import java.util.*;
 
 class AttackStrategy implements Strategy
 {
-
-    private boolean isCentered;
-    private boolean[] isThere;
-    private Player p;
-
-    public AttackStrategy(){}
-    
-    public AttackStrategy(boolean[] isThere)
+    private static int[] attackersID = {};
+    private static Location center;
+    private static boolean readyToAttack = false;
+    private static boolean de = false;
+    public AttackStrategy()
     {
-        this.isThere = isThere;
+        
     }
 
-    public void doTurn(PirateGame game, History history)
+    public void doTurn(PirateGame game,History history)
     {
-        game.debug(game.getTurn());
-        //isThere = new boolean[game.getMaxDronesCount()];
-        /*
-        if(game.getTurn() == 1){
-            game.debug("constructor");
-            isCentered = false;
-            isThere = new boolean[game.getMaxDronesCount()];
-            for(int i = 0; i < game.getMaxDronesCount(); i++)
-                {
-                    isThere[i] = false;
-                    game.debug(isThere);
-                }
-        }*/
-        p = game.getMyself();
-        game.debug("Activated: AttackStrategy");
-        Location center = new Location(game.getRowCount() / 2, game.getColCount() / 2);
-        List<Pirate> myLivingPirates = game.getMyLivingPirates();
-        List<Drone> myLivingDrones = game.getMyLivingDrones();
         
-        game.debug(isCentered);
-        isCentered = areAllTrue(isThere);
+        center = new Location(game.getRowCount()/2, game.getColCount()/2);
+        
+        
+        
+        isReadyToAttack(game);
+        
+        if(!de)
+        {
+        for (int i : attackersID) {
+            if(game.getMyDroneById(i) != null)
+                Mover.moveAircraft(game.getMyDroneById(i), game.getMyCities().get(0), game);
+        }
+        
+        
+        for(Drone drone : game.getMyLivingDrones())
+        {
+            boolean b = true;
+            for (int i : attackersID) {
+                if(drone.id==i)
+                    b=false;
+            }
+            game.debug(b);
+            if(b)
+                Mover.moveAircraft(drone, center, game);
+        }
         
         if(!game.getNeutralIslands().isEmpty())
-        {
-
-            game.debug("1");
-            for(int i = 0; i < myLivingPirates.size(); i++)
-            {
-                Pirate pirate = myLivingPirates.get(i);
-                Mover.moveAircraft(pirate, game.getNeutralIslands().get(0), game);
+            for (Pirate p : game.getMyLivingPirates()) {
+                if(!Attacker.tryAttack(p, game))
+                    Mover.moveAircraft(p, game.getNeutralIslands().get(0), game);
             }
+        else if(!readyToAttack)
+            for (Pirate p : game.getMyLivingPirates()) {
+                if(!Attacker.tryAttack(p, game))
+                    Mover.moveAircraft(p, center, game);
+            }
+        else
+            for (int i = 0 ; i< game.getMyLivingPirates().size() ; i++) {
+                if(!Attacker.tryAttack(game.getMyLivingPirates().get(i), game))
+                    Mover.moveAircraft(game.getMyLivingPirates().get(i), game.getMyCities().get(0), game);
+            }
+        if(game.GetMyDecoy()!=null)
+            Mover.moveAircraft(game.GetMyDecoy(), game.getMyCities().get(0), game);
         }
-        else if(game.getTurn() < 250)
+        de = false;
+    }
+    
+    
+    
+    
+    private static void isReadyToAttack(PirateGame game)
+    {
+        if(readyToAttack==true)
+            readyToAttack = OneAlive(attackersID,game);
+        else if(game.getMyLivingDrones().size() == game.getMaxDronesCount())
         {
-            game.debug("2");
-            ///
-            for(int i = 0; i < myLivingPirates.size(); i++)
-            {
-                Pirate pirate = myLivingPirates.get(i);
-                Mover.moveAircraft(pirate, center, game);
-
+            boolean b = true;
+            for (Drone drone : game.getMyLivingDrones()) {
+                if(drone.distance(center)!=0)
+                    b = false;
             }
-            game.debug(myLivingDrones.size());
-            for(int i = 0; i < myLivingDrones.size(); i++)
+            readyToAttack = b;
+            if (b)
             {
-                
-                Drone drone = myLivingDrones.get(i);
-                Mover.moveAircraft(drone, center, game);
-                if(drone.getLocation().compareTo(center) == 0)
+                if(game.getMyself().turnsToDecoyReload==0)
                 {
-                    isThere[i] = true;
-                    //game.debug(isThere[i]);
+                    game.decoy(game.getMyLivingPirates().get(0));
+                    de = true;
+                    
+                }
+                attackersID = new int[game.getMyLivingDrones().size()];
+                for (int i = 0 ; i < game.getMyLivingDrones().size() ; i++) {
+                    attackersID[i] = game.getMyLivingDrones().get(i).id;
                 }
             }
-            isCentered = areAllTrue(isThere);
-            
-        //game.debug("is1");
-        //game.debug(isCentered);
         
-        //game.debug(isCentered);
-        //game.debug("is2");    
-            ////
-        }else{
-            game.debug("3");
-            for(int i = 0; i < myLivingDrones.size(); i++)
-            {
-                Drone drone = myLivingDrones.get(i);
-                Mover.moveAircraft(drone, game.getMyCities().get(0).getLocation(), game);
-            }
-            
-            for(int i = 0; i < myLivingPirates.size(); i++)
-            {
-                Pirate pirate = myLivingPirates.get(i);
-                /*if(p.turnsToDecoyReload == 0 && i == 0)
-                {
-                   
-
-                else */
-                if(!Attacker.tryAttack(pirate, game))
-                {
-                    Drone closest = (Drone)Mover.getClosest(pirate.location,game.getMyLivingDrones());
-                    Mover.moveAircraft(pirate, new Location(closest.getLocation().row - 1,closest.getLocation().col - 3), game);
-                }
-
-            }
-            
-        
-        game.debug("isCentered");
-        game.debug(isCentered);
-        //end of the turn
-        /*for(int i = 0; i < game.getMaxDronesCount(); i++)
-            {
-                  
-            }*/
-            
         }
     }
-
-    private void initBoolArray(int size)
+    private static boolean OneAlive(int[] i,PirateGame game)
     {
-        isThere = new boolean[size];
-        if(!isCentered)
-        {
-            for(int i = 0; i < size; i++)
-            {
-                isThere[i] = false;
-            }
-        }
+        for (int j : i)
+           if(game.getMyDroneById(j) !=null)
+               return true;
+        return false;
+    
     }
-
-    private boolean areAllTrue(boolean[] array)
+    
+    
+    
+    
+    
+    
+    
+    public static void firstInit(PirateGame game)
     {
-        for(boolean b : array)
-        {
-        
-            if(!b)
-            {
-                return false;
-            }
-        }
-        return true;
+        center = new Location(game.getRowCount()/2, game.getColCount()/2);
     }
+    
+    
+    
+    
+    
+    
+    public static void moveListOfDroneByID(MapObject mapObject,int[] Ids,PirateGame game)
+    {
+        for (int id : Ids) {
+            Mover.moveAircraft(game.getMyDroneById(id), mapObject, game);
+        }
+    }
+    
+    
+    
+    
+    
+    public static boolean isInArray(Aircraft[] array,Aircraft a)
+    {
+        for (Aircraft aircraft : array) {
+            if(aircraft == a)
+                return true;
+        }
+        return false;
+    }
+    
 }
+
+
